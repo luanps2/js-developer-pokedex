fc7f9283882b6677de3a8a18a6e6aed37c74d7a2
Luan Costa PC <luan-esposito@hotmail.com>
2024-11-17 22:03:58 -0300

    pokedex finalizada.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,55 @@
-
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+function convertPokeApiDetailPokemon(pokeDetail){
+    const pokemon = new Pokemon();
+    
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+    const types = pokeDetail.types?.map((typeSlot) => typeSlot.type.name) || [];
+    const [type] = types;
 
-    pokemon.types = types
-    pokemon.type = type
+    const abilities = pokeDetail.abilities?.map((abilitieSlot) => abilitieSlot.ability.name) || [];
+    const [ability] = abilities;
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    const moves = pokeDetail.moves?.map((moveSlot) => moveSlot.move.name) || [];
+    const [move] = moves; 
 
-    return pokemon
+    pokemon.abilities = abilities;
+    pokemon.ability = ability;
+
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.moves = moves;
+    pokemon.move = move;
+
+    pokemon.photo = pokeDetail.sprites?.other?.dream_world?.front_default || pokeDetail.sprites?.front_default;
+    
+
+    return pokemon;
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+        .then(convertPokeApiDetailPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
+        .then((detailRequest) => Promise.all(detailRequest))
         .then((pokemonsDetails) => pokemonsDetails)
+        
 }
+
+// Promise.all([
+//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/4'),
+// ]).then((results) => console.log(results));
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,13 @@
-
 class Pokemon {
     number;
     name;
     type;
     types = [];
     photo;
+    ability;
+    abilities = [];
+    moves = [];
+    move;
+
 }
+
